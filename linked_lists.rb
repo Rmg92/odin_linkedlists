6b3f79f12f746b0b244af2f6fb33d07efda52777
Ricardo <ricardomg92@hotmail.com>
2022-04-12 21:46:27 +0200

    Create the methods required for the project inside the LinkedLists class

--- linked_lists.rb
+++ linked_lists.rb
@@ -1,7 +1,54 @@
 
 # this class represents the full list
 class LinkedList
-  
+  def append(value)
+    nil
+  end
+
+  def prepend(value)
+    nil
+  end
+
+  def size
+    nil
+  end
+
+  def head
+    nil
+  end
+
+  def tail
+    nil
+  end
+
+  def at(index)
+    nil
+  end
+
+  def pop
+    nil
+  end
+
+  def contains?(value)
+    nil
+  end
+
+  def find(value)
+    nil
+  end
+
+  def to_s
+    nil
+  end
+
+  # Extra Credit
+  def insert_at(value, index)
+    nil
+  end
+
+  def remove_at(index)
+    nil
+  end
 end
 
 # This class will contain the node value and pointer to the next node
