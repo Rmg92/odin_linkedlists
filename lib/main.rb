d82ddcae5b2aba589cfd39403b69a10b1dd99364
Ricardo <ricardomg92@hotmail.com>
2022-04-14 11:05:30 +0200

    Add tests for prepend and size methods

--- lib/main.rb
+++ lib/main.rb
@@ -4,6 +4,20 @@ list = LinkedList.new
 
 # Tests for append method
 list.append(1)
+puts list.size
 list.append(2)
+puts list.size
 list.append(3)
+puts list.size
 list.append(4)
+puts list.size
+
+# Tests for prepend method
+list.prepend(1)
+puts list.size
+list.prepend(2)
+puts list.size
+list.prepend(3)
+puts list.size
+list.prepend(4)
+puts list.size
