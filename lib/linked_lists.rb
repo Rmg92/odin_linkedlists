b00631cf20804a17de2adfbf2d68fa0ad13351c1
Ricardo <ricardomg92@hotmail.com>
2022-04-14 11:04:17 +0200

    Add size counting logic to the size method so it returns the size of the  list

--- lib/linked_lists.rb
+++ lib/linked_lists.rb
@@ -22,9 +22,7 @@ class LinkedList
       @tail = new_node
     else
       tmp_node = @head
-      until tmp_node.next_node.nil?
-        tmp_node = tmp_node.next_node
-      end
+      tmp_node = tmp_node.next_node until tmp_node.next_node.nil?
       @tail.next_node = new_node
       @tail = new_node
     end
@@ -33,14 +31,35 @@ class LinkedList
     p @tail
   end
 
-  def prepend(_value)
+  def prepend(value)
     # adds a new node containing value to the start of the list
-    nil
+    new_node = Node.new
+    new_node.value = value
+    if @head.nil?
+      @head = new_node
+    elsif @tail.nil?
+      @tail = @head
+      @head = new_node
+      @head.next_node = @tail
+    else
+      new_node.next_node = @head
+      @head = new_node
+    end
+    # For testing, erase when not needed
+    p @head
+    p @tail
   end
 
   def size
     # returns the total number of nodes in the list
-    nil
+    size = 1
+    0 if @head.nil?
+    tmp_node = @head
+    until tmp_node.next_node.nil?
+      tmp_node = tmp_node.next_node
+      size += 1
+    end
+    size
   end
 
   def head
@@ -80,12 +99,12 @@ class LinkedList
 
   # Extra Credit
   def insert_at(_value, _index)
-    # that inserts a new node with the provided value at the given index.
+    # inserts a new node with the provided value at the given index.
     nil
   end
 
   def remove_at(_index)
-    # that removes the node at the given index.
+    # removes the node at the given index.
     nil
   end
 end
