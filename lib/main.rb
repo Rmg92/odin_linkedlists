a7d9220e09649541b3f1f82677316af99a3f8d05
Ricardo <ricardomg92@hotmail.com>
2022-04-16 08:40:22 +0200

    Small refactoring

--- lib/main.rb
+++ lib/main.rb
@@ -4,61 +4,49 @@ require_relative 'linked_lists'
 
 list = LinkedList.new
 
-# Test size method
-puts list.size
-
 # Tests for append/size method
 list.append(1)
-puts list.size
+puts list.to_s
 list.append(2)
-puts list.size
-list.append(3)
-puts list.size
-list.append(4)
-puts list.size
+puts list.to_s
+list.append('Bye')
+puts list.to_s
+
 
 # Tests for prepend/size method
 list.prepend(1)
-puts list.size
+puts list.to_s
 list.prepend(2)
-puts list.size
-list.prepend(3)
-puts list.size
-list.prepend(4)
-puts list.size
+puts list.to_s
+list.prepend('Hello')
+puts list.to_s
 
 # Tests for head method
 puts list.head
+puts list.head.value
 
 # Tests for tail method
 puts list.tail
+puts list.tail.value
 
 # Tests for at method
 puts list.at(0)
 puts list.at(7)
-puts list.at(8)
 puts list.at(10)
 
 # Tests for pop method
-puts list.tail.value
 list.pop
-puts list.tail.value
-list.pop
-puts list.tail.value
-list.pop
-list.pop
-puts list.tail.value
+puts list.to_s
 
 # Tests for contains? method
 puts list.contains?(1)
 puts list.contains?(4)
-puts list.contains?(5)
+puts list.contains?('Hello')
 
 # Tests for find method
 puts list.find(5)
-puts list.find(3)
 puts list.find(1)
-puts list.find(4)
+puts list.find('Hello')
 
 # Tests for to_s method
 puts list.to_s
